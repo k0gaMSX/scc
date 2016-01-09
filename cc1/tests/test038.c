0e9ab695770b9c5501b56fb712527e1ed780950d
Roberto E. Vargas Caballero <k0ga@shike2.com>
2016-01-09 21:26:10 +0100

    Add solution for test of tentative declarations

--- cc1/tests/test038.c
+++ cc1/tests/test038.c
@@ -3,7 +3,25 @@
 name: TEST038
 description: Basic test for tentative definitions
 output:
-
+test038.c:45: error: redeclaration of 'x'
+G1	I	x
+(
+	#I0
+)
+F2	I	E
+X3	F2	main
+F4	P	E
+G5	F4	foo
+{
+\
+	r	X3	'P
+}
+G3	F2	main
+{
+\
+	G1	#I0	:I
+	r	G1
+}
 */
 
 int x;
@@ -24,3 +42,4 @@ main()
 	x = 0;
 	return x;
 }
+int x = 1;
