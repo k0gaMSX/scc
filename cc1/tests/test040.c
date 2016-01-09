dda0df0e38cf6b53d4db9b8858ee2dc0e07acc7f
Roberto E. Vargas Caballero <k0ga@shike2.com>
2016-01-09 22:28:24 +0100

    Add typedef fields to test040

--- cc1/tests/test040.c
+++ cc1/tests/test040.c
@@ -2,12 +2,25 @@
 name: TEST040
 description: Test for bug parsing typenames in struct definition
 output:
-
+test040.c:24: error: unexpected 'List'
+F8	I
+G9	F8	main
+{
+\
 */
 
 typedef struct List List;
 struct List {
 	int len;
 	struct List *head;
+	List *back;
 };
+
+int
+main(void)
+{
+	List List;
+
+	return List.len;
+}
 
