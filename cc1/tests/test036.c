0c1157ab7b327eb24b8692a5889ba5fe76a2aacc
Roberto E. Vargas Caballero <k0ga@shike2.com>
2016-01-11 10:16:01 +0100

    Add expected output to test037
    This is a very important moment in suckless life, we are now able to
    compile original Duff's device, whith k&r syntax.

--- cc1/tests/test036.c
+++ cc1/tests/test036.c
@@ -3,7 +3,51 @@
 name: TEST036
 description: Duff's device
 output:
-
+test036.c:60: warning: type defaults to 'int' in declaration
+F4	I	E
+G5	F4	send
+{
+R1	P	to
+R2	P	from
+R3	I	count
+\
+R7	I	n
+	R7	R3	#I7	+I	#I8	/I	:I
+	s	L9	R3	#I8	%I
+L10
+	e
+L11
+	R1	@I	R2	#N2	:iP	@I	:I
+L13
+	R1	@I	R2	#N2	:iP	@I	:I
+L14
+	R1	@I	R2	#N2	:iP	@I	:I
+L15
+	R1	@I	R2	#N2	:iP	@I	:I
+L16
+	R1	@I	R2	#N2	:iP	@I	:I
+L17
+	R1	@I	R2	#N2	:iP	@I	:I
+L18
+	R1	@I	R2	#N2	:iP	@I	:I
+L19
+	R1	@I	R2	#N2	:iP	@I	:I
+	j	L11	R7	#I1	:-I	#I0	>I
+	b
+L12
+	j	L8
+L9
+	t	#8
+	v	L19	#I1
+	v	L18	#I2
+	v	L17	#I3
+	v	L16	#I4
+	v	L15	#I5
+	v	L14	#I6
+	v	L13	#I7
+	v	L10	#I0
+L8
+}
 */
 
 /* Disgusting, no?  But it compiles and runs just fine.  I feel a combination of
