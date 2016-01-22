000323f401e35302f86538722eaa171809b66f1d
Roberto E. Vargas Caballero <k0ga@shike2.com>
2016-01-22 14:56:09 +0100

    Mark like wrong test059
    This test is not passed due to char s3[] = {"foo"};

--- cc1/tests/test059.c
+++ cc1/tests/test059.c
@@ -2,28 +2,9 @@
 name: TEST059
 description: Test of initalizers for strings
 error:
-test059.c:31: warning: initializer-string for array of chars is too long
-test059.c:33: error: array of inappropriate type initialized from string constant
+TODO
 output:
-V1	K	#4
-G2	V1	s0
-(
-	"666F6F00
-)
-V4	K	#7
-G5	V4	s1
-(
-	"666F6F00000000
-)
-V6	K	#2
-G7	V6	s2
-(
-	"666F
-)
-G9	P	p
-(
-	"666F6F00	'P
-)
+
 */
 
 char s0[] = "foo";
