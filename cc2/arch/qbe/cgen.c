a5ecfeb71efdc36674c5197b87289db4ec41135e
Roberto E. Vargas Caballero <k0ga@shike2.com>
2016-04-21 17:14:10 +0200

    [cc2-qbe] Add support for operations in long and for floats
    This commits add 4 tables, one for w, one for l, one for s
    and one for d. We need these tables because we don't have
    the semantic information about the types in the address.

--- cc2/arch/qbe/cgen.c
+++ cc2/arch/qbe/cgen.c
@@ -9,24 +9,84 @@ enum sflags {
 	ISCONS = 2
 };
 
-static char opasm[] = {
-	[OADD] = ASADD,
-	[OSUB] = ASSUB,
-	[OMUL] = ASMUL,
-	[OMOD] = ASMOD,
-	[ODIV] = ASDIV,
-	[OSHL] = ASSHL,
-	[OSHR] = ASSHR,
-	[OLT] = ASLT,
-	[OGT] = ASGT,
-	[OLE] = ASLE,
-	[OGE] = ASGE,
-	[OEQ] = ASEQ,
-	[ONE] = ASNE,
-	[OBAND] = ASBAND,
-	[OBOR] = ASBOR,
-	[OBXOR] = ASBXOR,
-	[OCPL] = ASCPL
+static char opasmw[] = {
+	[OADD] = ASADDW,
+	[OSUB] = ASSUBW,
+	[OMUL] = ASMULW,
+	[OMOD] = ASMODW,
+	[ODIV] = ASDIVW,
+	[OSHL] = ASSHLW,
+	[OSHR] = ASSHRW,
+	[OLT] = ASLTW,
+	[OGT] = ASGTW,
+	[OLE] = ASLEW,
+	[OGE] = ASGEW,
+	[OEQ] = ASEQW,
+	[ONE] = ASNEW,
+	[OBAND] = ASBANDW,
+	[OBOR] = ASBORW,
+	[OBXOR] = ASBXORW,
+	[OCPL] = ASCPLW
+};
+
+static char opasml[] = {
+	[OADD] = ASADDL,
+	[OSUB] = ASSUBL,
+	[OMUL] = ASMULL,
+	[OMOD] = ASMODL,
+	[ODIV] = ASDIVL,
+	[OSHL] = ASSHLL,
+	[OSHR] = ASSHRL,
+	[OLT] = ASLTL,
+	[OGT] = ASGTL,
+	[OLE] = ASLEL,
+	[OGE] = ASGEL,
+	[OEQ] = ASEQL,
+	[ONE] = ASNEL,
+	[OBAND] = ASBANDL,
+	[OBOR] = ASBORL,
+	[OBXOR] = ASBXORL,
+	[OCPL] = ASCPLL
+};
+
+static char opasms[] = {
+	[OADD] = ASADDS,
+	[OSUB] = ASSUBS,
+	[OMUL] = ASMULS,
+	[OMOD] = ASMODS,
+	[ODIV] = ASDIVS,
+	[OSHL] = ASSHLS,
+	[OSHR] = ASSHRS,
+	[OLT] = ASLTS,
+	[OGT] = ASGTS,
+	[OLE] = ASLES,
+	[OGE] = ASGES,
+	[OEQ] = ASEQS,
+	[ONE] = ASNES,
+	[OBAND] = ASBANDS,
+	[OBOR] = ASBORS,
+	[OBXOR] = ASBXORS,
+	[OCPL] = ASCPLS
+};
+
+static char opasmd[] = {
+	[OADD] = ASADDD,
+	[OSUB] = ASSUBD,
+	[OMUL] = ASMULD,
+	[OMOD] = ASMODD,
+	[ODIV] = ASDIVD,
+	[OSHL] = ASSHLD,
+	[OSHR] = ASSHRD,
+	[OLT] = ASLTD,
+	[OGT] = ASGTD,
+	[OLE] = ASLED,
+	[OGE] = ASGED,
+	[OEQ] = ASEQD,
+	[ONE] = ASNED,
+	[OBAND] = ASBANDD,
+	[OBOR] = ASBORD,
+	[OBXOR] = ASBXORD,
+	[OCPL] = ASCPLD
 };
 
 static Node *
@@ -63,6 +123,7 @@ cgen(Node *np)
 	Symbol *sym;
 	Type *tp;
 	int op;
+	char *tbl;
 
 	if (!np)
 		return NULL;
@@ -81,10 +142,6 @@ cgen(Node *np)
 	case OMEM:
 	case OAUTO:
 		return np;
-	case OEQ:
-	case ONE:
-		op = opasm[np->op] + (tp->size == 8);
-		goto binary;
 	case OADD:
 	case OSUB:
 	case OMUL:
@@ -100,7 +157,19 @@ cgen(Node *np)
 	case OBOR:
 	case OBXOR:
 	case OCPL:
-		op = opasm[np->op];
+	case OEQ:
+	case ONE:
+		switch (tp->size) {
+		case 4:
+			tbl = (tp->flags & INTF) ? opasmw : opasms;
+			break;
+		case 8:
+			tbl = (tp->flags & INTF) ? opasml : opasmd;
+			break;
+		default:
+			abort();
+		}
+		op = tbl[np->op];
 	binary:
 		if ((l->flags & (ISTMP|ISCONS)) == 0)
 			l = np->left = load(l);
