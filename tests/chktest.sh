951fc8bd3252dfbde5fd682f5b373746400980fa
Roberto E. Vargas Caballero <k0ga@shike2.com>
2016-06-21 13:01:44 +0200

    [test] Use $@ instead of using current directory
    This form of the command is more general because we can run
    the tests that are needed, and no all the tests in the directory.

--- tests/chktest.sh
+++ tests/chktest.sh
@@ -4,10 +4,10 @@ trap 'tabs -8' 0 1 2 3 15
 tabs 40
 ulimit -c 0
 
-for i in *.c
+for i in $@
 do
 	printf "%s\t" $i
 	(set -e
-	 scc -m qbe $i && ./a.out
+	 scc -m qbe "$i" && ./a.out
 	) 2>/dev/null && echo [OK] || echo [FAILED]
 done
