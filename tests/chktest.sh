db420540ad48bc0303ed2be999d0e5d0858df9f8
Roberto E. Vargas Caballero <k0ga@shike2.com>
2016-06-23 11:46:40 +0200

    [tests] Improve chktest.sh
    Simplify the subshell, and remove the temporal files created.

--- tests/chktest.sh
+++ tests/chktest.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-trap 'tabs -8' 0 1 2 3 15
+trap 'tabs -8;rm -f a.out' 0 1 2 3 15
 tabs 40
 ulimit -c 0
 
 for i in $@
 do
 	printf "%s\t" $i
-	(set -e
-	 scc -m qbe "$i" && ./a.out
-	) 2>/dev/null && echo [OK] || echo [FAILED]
+	(scc -m qbe "$i" && ./a.out) 2>/dev/null && echo [OK] || echo [FAILED]
 done
