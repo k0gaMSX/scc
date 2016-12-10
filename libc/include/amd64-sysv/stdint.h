9efb947d3ec79570f070e89c0020490f5ffa43d4
Roberto E. Vargas Caballero <k0ga@shike2.com>
2016-12-10 08:34:07 +0100

    [libc] Add stdint.h
    This header is used for musl libc headers, which at the moment
    is the current target of the testing for scc.

---
+++ libc/include/amd64-sysv/stdint.h
@@ -0,0 +1,41 @@
+/* See LICENSE file for copyright and license details. */
+#ifndef _STDINT_H_
+#define _STDINT_H_
+
+typedef int8_t signed char;
+typedef int16_t short;
+typedef int32_t int;
+typedef int64_t long;
+
+typedef uint8_t unsigned signed char;
+typedef uint16_t unsigned short;
+typedef uint32_t unsigned int;
+typedef uint64_t unsigned long;
+
+typedef int8_least_t signed char;
+typedef int16_least_t short;
+typedef int32_least_t int;
+typedef int64_least_t long;
+
+typedef uint8_least_t unsigned signed char;
+typedef uint16_least_t unsigned short;
+typedef uint32_least_t unsigned int;
+typedef uint64_least_t unsigned long;
+
+typedef int8_fast_t int;
+typedef int16_fast_t int;
+typedef int32_fast_t int;
+typedef int64_fast_t long;
+
+typedef int8_fast_t unsigned;
+typedef int16_fast_t unsigned;
+typedef int32_fast_t unsigned;
+typedef int64_fast_t unsigned long;
+
+typedef long intptr_t;
+typedef unsigned long uintptr_t;
+
+typedef long intmax_t;
+typedef unsigned long uintmax_t;
+
+#endif
---
+++ libc/include/z80/stdint.h
@@ -0,0 +1,41 @@
+/* See LICENSE file for copyright and license details. */
+#ifndef _STDINT_H_
+#define _STDINT_H_
+
+typedef int8_t signed char;
+typedef int16_t int;
+typedef int32_t long;
+typedef int64_t long long;
+
+typedef uint8_t unsigned signed char;
+typedef uint16_t unsigned;
+typedef uint32_t unsigned long;
+typedef uint64_t unsigned long long;
+
+typedef int8_least_t signed char;
+typedef int16_least_t int;
+typedef int32_least_t long;
+typedef int64_least_t long long;
+
+typedef uint8_least_t unsigned char;
+typedef uint16_least_t unsigned;
+typedef uint32_least_t unsigned long;
+typedef uint64_least_t unsigned long long;
+
+typedef int8_fast_t signed char;
+typedef int16_fast_t int;
+typedef int32_fast_t long;
+typedef int64_fast_t long long;
+
+typedef int8_fast_t unsigned char;
+typedef int16_fast_t unsigned;
+typedef int32_fast_t unsigned long;
+typedef int64_fast_t unsigned long long;
+
+typedef long intptr_t;
+typedef unsigned uintptr_t;
+
+typedef long intmax_t;
+typedef unsigned long long uintmax_t;
+
+#endif
