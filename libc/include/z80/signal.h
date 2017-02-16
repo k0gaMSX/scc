ffe3e9b4a391cb17088cff36d9449fe7bd47a796
Roberto E. Vargas Caballero <k0ga@shike2.com>
2017-02-16 17:04:13 +0100

    [libc] Move signal.h to the common directory
    At this point signal.h hasn't specific bits, so
    it can be ported directly.

--- libc/include/z80/signal.h
+++
@@ -1,8 +0,0 @@
-/* See LICENSE file for copyright and license details. */
-#ifndef _SIGNAL_H
-#define _SIGNAL_H
-
-void ( *signal(int signum, void (*handler)(int)) ) (int);
-int raise(int sig);
-
-#endif
