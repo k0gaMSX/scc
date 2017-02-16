8983432321fac3cb410bb19beae46d891c438328
Roberto E. Vargas Caballero <k0ga@shike2.com>
2017-02-16 20:53:38 +0100

    [libc] Add strcpy()

---
+++ libc/src/strcpy.c
@@ -0,0 +1,13 @@
+/* See LICENSE file for copyright and license details. */
+
+#include <string.h>
+
+char *
+strcpy(char *dst, const char *src)
+{
+	char *ret = dst;
+
+	while (*dst++ = *src++)
+		/* nothing */;
+	return ret;
+}
