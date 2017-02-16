3f51e82f5bad60cd837488bd5e1a8badd3441835
Roberto E. Vargas Caballero <k0ga@shike2.com>
2017-02-16 21:17:30 +0100

    [libc] Add strchr()

---
+++ libc/src/strchr.c
@@ -0,0 +1,11 @@
+/* See LICENSE file for copyright and license details. */
+
+#include <string.h>
+
+char *
+strchr(const char *s, int c)
+{
+	while (*s && *s != c)
+		++s;
+	return (*s) ? (char *) s : NULL;
+}
