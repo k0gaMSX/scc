6cdd955f1d50996aa6b411756910e813f591e2f0
Quentin Rameau <quinq@fifth.space>
2017-02-17 00:21:50 +0100

    [libc] Fix strncpy
    Don't forget n is unsigned.

--- libc/src/strncpy.c
+++ libc/src/strncpy.c
@@ -7,8 +7,8 @@ strncpy(char *dst, const char *src, size_t n)
 {
 	char *ret = dst;
 
-	while (n-- > 0 && (*dst++ = *src++))
-		/* nothing */;
+	for (; n > 0 && *src; --n)
+		*dst++ = *src++;
 	while (n-- > 0)
 		*dst++ = '\0';
 	return ret;
