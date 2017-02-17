5623808ee73aea1ceffcf3503def2ec3f4773e1a
Roberto E. Vargas Caballero <k0ga@shike2.com>
2017-02-17 08:47:08 +0100

    [libc] Add memchr()

---
+++ libc/src/memchr.c
@@ -0,0 +1,13 @@
+/* See LICENSE file for copyright and license details. */
+
+#include <string.h>
+
+void *
+memchr(const void *s, int c, size_t n)
+{
+	unsigned char *bp = (char *) s;
+
+	while (n > 0 && *bp++ != c)
+		--n;
+	return (n == 0) ? NULL : bp-1;
+}
