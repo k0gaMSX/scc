b3b28414fd4050514ac583ba72060574d74a1bab
Quentin Rameau <quinq@fifth.space>
2017-02-17 12:26:07 +0100

    [libc] Fix memcmp return value

--- libc/src/memcmp.c
+++ libc/src/memcmp.c
@@ -7,7 +7,7 @@ memcmp(const void *s1, const void *s2, size_t n)
 {
 	char *s = (char *) s1, *t = (char *) s2;
 
-	while (n > 0 && *s++ != *t++)
-		--n;
-	return n != 0;
+	while (n > 0 && *s == *t)
+		--n, ++s, ++t;
+	return n ? (*s - *t) : 0;
 }
