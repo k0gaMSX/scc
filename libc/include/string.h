7def427f50ce226527e40191d38057ad68856570
Quentin Rameau <quinq@fifth.space>
2017-02-21 17:31:00 +0100

    [libc] Complete string.h

--- libc/include/string.h
+++ libc/include/string.h
@@ -4,26 +4,27 @@
 
 #include <arch/string.h>
 
-extern char *strcpy(char *dst, const char *src);
-extern char *strncpy(char *dst, const char *src, size_t n);
-extern char *strcat(char *dst, const char *src);
-extern char *strncat(char *dst, const char *src, size_t n);
-extern size_t strxfrm(char *dst, const char *src, size_t n);
-extern size_t strlen(const char *s);
+extern void *memcpy(void * restrict s1, const void * restrict s2, size_t n);
+extern void *memmove(void *s1, const void *s2, size_t n);
+extern char *strcpy(char * restrict s1, const char * restrict s2);
+extern char *strncpy(char * restrict s1, const char * restrict s2, size_t n);
+extern char *strcat(char * restrict s1, const char * restrict s2);
+extern char *strncat(char * restrict s1, const char * restrict s2, size_t n);
+extern int memcmp(const void *s1, const void *s2, size_t n);
 extern int strcmp(const char *s1, const char *s2);
 extern int strcoll(const char *s1, const char *s2);
-extern char *strchr(const char *s, int c);
-extern char *strrchr(const char *s, int c);
-extern size_t strspn(const char *s, const char *accept);
-extern size_t strcspn(const char *s, const char *reject);
-extern size_t strpbrk(const char *s, const char *accept);
-extern size_t strstr(const char *s, const char *sub);
-extern char *strtok(const char *s, const char *delim);
-
-extern void *memset(void *s, int c, size_t n);
-extern void *memcpy(void *dst, const void *src, size_t n);
-extern void *memmove(void *dst, const void *src, size_t n);
-extern int memcmp(const void *s1, const void *s2, size_t n);
+extern int strncmp(const char *s1, const char *s2, size_t n);
+extern size_t strxfrm(char * restrict s1, const char * restrict s2, size_t n);
 extern void *memchr(const void *s, int c, size_t n);
+extern char *strchr(const char *s, int c);
+extern size_t strcspn(const char *s1, const char *s2);
+extern char *strpbrk(const char *s1, const char *s2);
+extern char *strrchr(const char *s, int c);
+extern size_t strspn(const char *s1, const char *s2);
+extern char *strstr(const char *s1, const char *s2);
+extern char *strtok(char * restrict s1, const char * restrict s2);
+extern void *memset(void *s, int c, size_t n);
+extern char *strerror(int errnum);
+extern size_t strlen(const char *s);
 
 #endif
