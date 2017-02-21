0f9980fc8dbb5d51b6442f892aa2c31a9c05c768
Quentin Rameau <quinq@fifth.space>
2017-02-21 18:28:19 +0100

    [libc] Add time.h

---
+++ libc/include/bits/z80/arch/time.h
@@ -0,0 +1,11 @@
+/* See LICENSE file for copyright and license details. */
+#ifndef NULL
+#define NULL ((void *) 0)
+#endif
+
+#ifndef _SIZET
+typedef unsigned size_t;
+#define _SIZET
+#endif
+
+typedef long time_t;
