714865e3490220bae41784c95e0c99c9fcff11d7
Quentin Rameau <quinq@fifth.space>
2017-02-21 17:48:50 +0100

    [libc] Add strncmp

---
+++ libc/src/strncmp.c
@@ -0,0 +1,11 @@
+/* See LICENSE file for copyright and license details. */
+
+#include <string.h>
+
+int
+strncmp(const char *s1, const char *s2, size_t n)
+{
+	for (; n && *s1 && *s2 && *s1 != *s2; --n, ++s1, ++s2);
+		/* nothing */;
+	return n ? (*(unsigned char *)s1 - *(unsigned char *)s2) : 0;
+}
