23dd58a0a616e7695b58073349762e104cf604c2
Roberto E. Vargas Caballero <k0ga@shike2.com>
2017-02-23 16:39:23 +0100

    [tests] Add strpbr()

---
+++ libc/src/strpbrk.c
@@ -0,0 +1,18 @@
+/* See LICENSE file for copyright and license details. */
+
+#include <string.h>
+
+char *
+strpbrk(const char *s1, const char *s2)
+{
+	int c;
+	const char *p;
+
+	for (; c = *s1; ++s1) {
+		for (p = s2; *p && *p != c; ++p)
+			/* nothing */;
+		if (*p == c)
+			return s1;
+	}
+	return NULL;
+}
