494bd039510a349f198a1cd310b8e36ed8ddce09
Roberto E. Vargas Caballero <k0ga@shike2.com>
2017-02-23 15:16:38 +0100

    [libc] Add strstr()

---
+++ libc/src/strstr.c
@@ -0,0 +1,27 @@
+/* See LICENSE file for copyright and license details. */
+
+#include <string.h>
+
+char *
+strstr(const char *s1, const char *s2)
+{
+	const char *p, *q;
+	int c;
+
+	c = *s2++;
+	if (c == '\0')
+		return (char *) s1;
+
+	while (*s1) {
+		if (*s1 != c) {
+			++s1;
+		} else {
+			p = s1++;
+			for (q = s2; *q && *s1 == *q; ++s1, ++q)
+				/* nothing */;
+			if (*q == '\0')
+				return (char *) p;
+		}
+	}
+	return NULL;
+}
