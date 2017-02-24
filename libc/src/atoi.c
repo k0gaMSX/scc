e167a1b4e19b2911a3fd1b0e97538034b421e6b6
Christopher M. Graff <cm0graff@gmail.com>
2017-02-24 05:53:17 -0600

    [libc] Add atoi

---
+++ libc/src/atoi.c
@@ -0,0 +1,28 @@
+/* See LICENSE file for copyright and license details. */
+
+#include <ctype.h>
+#include <stdlib.h>
+
+int
+atoi(const char *s)
+{
+	int n, sign = 1;
+
+	while(isspace(*s))
+		++s;
+
+	switch(*s) {
+	case '-':
+		sign = -1;
+	case '+':
+		++s;
+	default:
+		break;
+	}
+
+	for (n = 0; isdigit(*s); ++s)
+		n = 10 * n + (*s - '0');
+
+	return sign * n;
+}
+
