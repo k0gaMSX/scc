45f79f6bb8623c3b169480c54c860fb0eb4487da
Quentin Rameau <quinq@fifth.space>
2017-02-27 15:57:48 +0100

    [libc] Correct style in strcspn

--- libc/src/strcspn.c
+++ libc/src/strcspn.c
@@ -5,15 +5,15 @@
 size_t
 strcspn(const char *s1, const char *s2)
 {
-        size_t n;
-        int c;
+	size_t n;
+	int c;
 	const char *p;
 
-        for (n = 0; c = *s1++; ++n) {
-                for (p = s2; *p && *p != c; ++p)
-                        /* nothing */;
-                if (*p == c)
-                        break;
-        }
-        return n;
+	for (n = 0; c = *s1++; ++n) {
+		for (p = s2; *p && *p != c; ++p)
+			/* nothing */;
+		if (*p == c)
+			break;
+	}
+	return n;
 }
