11f3b95b620bb62279800a648d6cb9c736be78d2
Christopher M. Graff <cm0graff@gmail.com>
2017-02-28 11:56:45 -0600

    [libc] Fix typo in strncmp

--- libc/src/strncmp.c
+++ libc/src/strncmp.c
@@ -5,7 +5,7 @@
 int
 strncmp(const char *s1, const char *s2, size_t n)
 {
-	for (; n && *s1 && *s2 && *s1 != *s2; --n, ++s1, ++s2);
+	for (; n && *s1 && *s2 && *s1 == *s2; --n, ++s1, ++s2);
 		/* nothing */;
 	return n ? (*(unsigned char *)s1 - *(unsigned char *)s2) : 0;
 }
