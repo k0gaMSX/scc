b5225423a6f068c95a96c56690b5fb2ecf692df3
Quentin Rameau <quinq@fifth.space>
2017-03-11 12:05:54 +0100

    [tests] Write file directly with ed in chktests.sh

--- tests/error/chktest.sh
+++ tests/error/chktest.sh
@@ -11,11 +11,11 @@ rm -f test.log
 while read i state
 do
 	echo $i >> test.log
-	printf "%s\t" $i
-	printf "%s" $state
+	printf "%s\t" "$i"
+	printf "%s" "$state"
 
 	scc $CFLAGS -w -c "$i" 2> $err
-	echo '/^PATTERN/+;/^\./-p' | ed -s $i > $chk
-	diff -c $chk $err >> test.log  && echo [OK] || echo [FAILED]
+	echo "/^PATTERN/+;/^\./-w \"$chk\"" | ed -s "$i"
+	diff -c "$chk" "$err" >> test.log  && echo [OK] || echo [FAILED]
 	rm -f *.o
 done
