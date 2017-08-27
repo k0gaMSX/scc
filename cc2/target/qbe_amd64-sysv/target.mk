9f46cf48eaf3a14f7983f343fc3f7dee47441876
Quentin Rameau <quinq@fifth.space>
2017-08-27 21:19:21 +0200

    [cc2] Fix typo in qbe_amd64-sysv/target.mk

--- cc2/target/qbe_amd64-sysv/target.mk
+++ cc2/target/qbe_amd64-sysv/target.mk
@@ -3,4 +3,4 @@ OBJ-qbe_amd64-sysv = $(OBJ)  \
         target/qbe/cgen.o \
         target/qbe/optm.o \
         target/qbe/code.o \
-        target/arm64-sysv/types.o
+        target/amd64-sysv/types.o
