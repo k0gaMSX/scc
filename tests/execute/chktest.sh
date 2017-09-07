4e728dc6a09b4fbfb6f6406a743b50f4306b1f6e
Roberto E. Vargas Caballero <k0ga@shike2.com>
2017-09-07 10:51:33 +0100

    [tests] Force to use hard tabs
    tabs doesn't work if the tty is not configured to use hardware tabs.
    In this patch is also included an additional quoting for [] characters.

--- tests/execute/chktest.sh
+++ tests/execute/chktest.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-trap 'tabs -8;rm -f a.out; exit 1' 0 1 2 3 15
+ttyflags=`stty -g`
+trap "stty $ttyflags;tabs -8;rm -f a.out; exit 1" 0 1 2 3 15
+stty tabs
 tabs 40
 ulimit -c 0
 rm -f test.log
@@ -12,5 +14,5 @@ do
 	printf "%s" $state
 	rm -f a.out
 	(scc -Isysinclude $CFLAGS "$i" && ./a.out) 2>test.log &&
-		echo [OK] || echo [FAILED]
+		echo "[OK]" || echo "[FAILED]"
 done
