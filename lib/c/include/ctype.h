fbda8879666e4144c62bcab1bd6530dac1ea09c0
Roberto E. Vargas Caballero <k0ga@shike2.com>
2017-09-25 10:47:12 +0100

    [libc] Add macro definitions for tolower() and toupper()
    The library is not thread safe in a lot of different places, and it
    doesn't try to be thread safe because it will require a more complex
    design, out of the scope of this simple library. For this reason
    it is not a problem to use a global static variable.

--- lib/c/include/ctype.h
+++ lib/c/include/ctype.h
@@ -28,6 +28,7 @@ extern int toupper(int c);
 #define _SP	0x80	/* hard space (0x20) */
 
 extern unsigned char __ctype[];
+extern int __ctmp;
 
 #define isalnum(c)  ((__ctype+1)[(c)] & (_U|_L|_D))
 #define isalpha(c)  ((__ctype+1)[(c)] & (_U|_L))
@@ -40,6 +41,9 @@ extern unsigned char __ctype[];
 #define isspace(c)  ((__ctype+1)[(c)] & (_S))
 #define isupper(c)  ((__ctype+1)[(c)] & (_U))
 #define isxdigit(c) ((__ctype+1)[(c)] & (_D|_X))
+
+#define tolower(c) ((__ctmp=c, isupper(__ctmp) ? __ctmp | 0x20 : __ctmp))
+#define toupper(c) ((__ctmp=c, islower(__ctmp) ? __ctmp & ~0x20 : __ctmp))
 
 #define isascii(c) ((unsigned)(c)<=0x7f)
 
