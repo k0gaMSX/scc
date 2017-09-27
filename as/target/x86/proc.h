b0f91c40ff3f623c03b1ecbe5f929d3ef0b1a063
Roberto E. Vargas Caballero <k0ga@shike2.com>
2017-09-27 08:04:40 +0200

    [as] Extend list of intel registers

--- as/target/x86/proc.h
+++ as/target/x86/proc.h
@@ -7,32 +7,49 @@ enum args {
         AIMM32,
         AIMM64,
 
+	AREG_CS,
+	AREG_DS,
+	AREG_SS,
+	AREG_ES
+	AREG_FS,
+	AREG_GS,
+
+	AREG_EFLAGS,
+
 	AREG_AX,
 	AREG_AL,
 	AREG_AH,
 	AREG_EAX,
+	AREG_RAX,
 
-	AREG_BC,
+	AREG_BX,
 	AREG_BL,
 	AREG_BH,
 	AREG_EBX,
+	AREG_RBX,
 
 	AREG_CX,
 	AREG_CL,
 	AREG_CH,
 	AREG_ECX,
+	AREG_RCX,
 
 	AREG_DX,
 	AREG_DL,
 	AREG_DH,
 	AREG_EDX,
+	AREG_RDX,
 
 	AREG_SI,
 	AREG_DI,
 
 	AREG_SP,
 	AREG_ESP,
+	AREG_RSP,
+
+	AREG_BP,
 	AREG_EBP,
+	AREG_RBP,
 
         AREP, 
 };
