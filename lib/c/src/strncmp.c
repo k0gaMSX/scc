1dcc4bd503a9705419f3405e8db92019a7e02c26
Roberto E. Vargas Caballero <k0ga@shike2.com>
2017-12-07 09:38:51 +0000

    [lib/c] Fix small typo in strncmp()
    We want equal strings, no different strings

--- lib/c/src/strncmp.c
+++ lib/c/src/strncmp.c
@@ -8,7 +8,7 @@ strncmp(const char *s1, const char *s2, size_t n)
 
 	if (n == 0)
 		return 0;
-	while ((c = *s1) != '\0' && c != *s2) {
+	while ((c = *s1) != '\0' && c == *s2) {
 		if (--n == 0)
 			return 0;
 		++s1, ++s2;
