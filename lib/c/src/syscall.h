61aee6d755efe40baee53f12242cb3199949fc8e
Roberto E. Vargas Caballero <k0ga@shike2.com>
2017-12-10 18:49:53 +0100

    [lib/c] Add tmpnam()

--- lib/c/src/syscall.h
+++ lib/c/src/syscall.h
@@ -5,6 +5,10 @@ extern int _read(int fd, void *buf, size_t n);
 extern int _write(int fd, void *buf, size_t n);
 extern int _lseek(int fd, long off, int whence);
 extern void _Exit(int status);
+extern void _access(char *path, int mode);
+
 extern int raise(int sig);
 extern void (*signal(int sig, void (*func)(int)))(int);
 extern getenv(const char *var);
+extern int rename(const char *from, const char *to);
+extern int remove(const char *path);
