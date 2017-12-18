08d465302ea0806f1c0b95b873d5725592e1e8e3
Roberto E. Vargas Caballero <k0ga@shike2.com>
2017-12-18 23:01:30 +0100

    [as] Add test.sh script
    This script is added to help etsting that the code is doing the
    correct work.

---
+++ as/test.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+tmp1=`mktemp`
+tmp2=`mktemp`
+
+trap "rm -f a.out $tmp1 $tmp2" 0 2 3 
+
+./as-$cpu target/$cpu/test.s
+
+sed -n '/^\// ! {
+	s/.*\/ //
+	s/  */\
+/
+	w '$tmp1'
+}' target/$cpu/test.s
+
+../objdump/objdump |
+sed -n '/^data:/,$ {
+	/^data:/ ! {
+		s/.*: //
+		s/  */\
+/
+		w '$tmp2'
+	}
+}' 
+
+diff $tmp1 $tmp2 >/dev/null
