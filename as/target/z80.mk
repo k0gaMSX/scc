0dfc9769e7eb4e2f8956e7328841c19eb6845587
Roberto E. Vargas Caballero <k0ga@shike2.com>
2018-01-25 12:28:33 +0100

    [as] Run sort under the C locale
    POSIX sort is so stupid that it compares using the current locale.
    It means that the order of the instructions depend of the locale.

--- as/target/z80.mk
+++ as/target/z80.mk
@@ -4,6 +4,6 @@ Z80_LST = target/x80/z80tbl.o target/x80/z80.o target/x80/ins.o
 $(Z80_LST): target/x80/proc.h
 
 target/x80/z80tbl.c: gentbl.awk target/x80/x80.dat target/x80/rules.dat
-	./gentbl.sh -f x80 -c z80
+	LC_ALL=C ./gentbl.sh -f x80 -c z80
 
 OBJ-z80 = $(OBJ) $(Z80_LST)
